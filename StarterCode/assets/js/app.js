0ec97b04e167269ced9d27cbab1f6c95e73d07c9
gina <ginac04@gmail.com>
2020-05-27 22:19:16 -0500

    added abbreviations and fixed tooltips

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,31 +1,34 @@
+// Define SVG area dimensions
 var svgWidth = 960;
 var svgHeight = 500;
 
+// Define the chart's margins as an object
 var margin = {
     top: 20,
     right: 40,
-    bottom: 60,
+    bottom: 80,
     left: 100
 };
 
+// Define dimensions of the chart area
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
-// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+// Select scatter div, append SVG area to it, and set the dimensions
 var svg = d3.select("#scatter")
     .append("svg")
     .attr("width", svgWidth)
     .attr("height", svgHeight);
 
+// Append a group to the SVG area and shift ('translate') it to the right and down to adhere to the margins set in the "chartMargin" object.    
 var chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-// Import Data
+// Load data from the data.csv file
 (async function() {
     var censusData = await d3.csv("assets/data/data.csv").catch(function(error) {
         console.log(error);
     });
-
 
     // Step 1: Parse Data/Cast as numbers
     // ==============================
@@ -62,25 +65,26 @@ var chartGroup = svg.append("g")
     // ==============================
     var circlesGroup = chartGroup.selectAll("circle")
         .data(censusData)
-        .enter()
-        .append("circle")
+        .join("circle")
         .attr("cx", d => xLinearScale(d.poverty))
         .attr("cy", d => yLinearScale(d.healthcare))
         .attr("r", "15")
         .attr("fill", "darkcyan")
-        .attr("opacity", "0.5");
+        .attr("opacity", "0.5")
 
-
-    // Step 5: Label circles
+    // Step 5: Label Circles
     // ==============================
-    var circleLabels = chartGroup.selectAll("circle")
+    var node = chartGroup.selectAll('.nodes')
         .data(censusData)
-        .enter()
-        .attr("class", "stateText")
-        .text(function(d) {
-            return d.abbr;
+        .join('g')
+        .attr('class', 'nodes')
 
-        });
+    node.append("text")
+        .attr("dx", d => xLinearScale(d.poverty))
+        .attr("dy", d => yLinearScale(d.healthcare) + 5)
+        .attr('text-anchor', 'middle')
+        .attr('fill', "white")
+        .text(d => d.abbr);
 
     // Step 7: Initialize tool tip
     // ==============================
@@ -93,11 +97,11 @@ var chartGroup = svg.append("g")
 
     // Step 8: Create tooltip in the chart
     // ==============================
-    chartGroup.call(toolTip);
+    node.call(toolTip);
 
     // Step 9: Create event listeners to display and hide the tooltip
     // ==============================
-    circlesGroup.on("click", function(data) {
+    node.on("click", function(data) {
             toolTip.show(data, this);
         })
         // onmouseout event
@@ -105,17 +109,18 @@ var chartGroup = svg.append("g")
             toolTip.hide(data);
         });
 
-    // Create axes labels
+    // Step 10: Create axes labels
+    // ==============================
     chartGroup.append("text")
         .attr("transform", "rotate(-90)")
         .attr("y", 0 - margin.left + 40)
         .attr("x", 0 - (height / 2))
         .attr("dy", "1em")
         .attr("class", "aText")
-        .text("Healthcare %");
+        .text("Lacks Healthcare (%)");
 
     chartGroup.append("text")
         .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
         .attr("class", "aText")
-        .text("Poverty %");
+        .text(" In Poverty (%)");
 })()
